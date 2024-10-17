d456dbf957e12d57250f322bc82c9089c0f7789a
Daniil Ivashchev <ivasevdaniil@gmail.com>
2024-10-17 14:06:04 +0300

    feat: add function to join like child person

--- controllers/affController.php
+++ controllers/affController.php
@@ -37,25 +37,23 @@ class affController extends baseController {
      * СТРАНИЦА ПАРТНЁРА В ЛК
      */
     public function actionAff()
-    {     
+    {
 
 
         $extension = System::CheckExtensension('partnership', 1);
         if (!$extension) {
             require_once ("{$this->template_path}/404.php");
         }
-        
+
         // Проверка авторизации
         $userId = User::checkLogged();
-        
-      
-         
+
         // Данные юзера
         $user = User::getUserById($userId);
         if ($user['is_partner'] != 1) {
             System::redirectUrl('/lk');
         }
-		
+
 		if (isset($_POST['save_postback'])) {
             $postback = json_encode($_POST['postback']);
             $fb_pixel = isset($_POST['fb_pixel']) ? json_encode($_POST['fb_pixel']) : null;
@@ -71,6 +69,87 @@ class affController extends baseController {
                 TelegramProduct::addOrUpdate(  $product_id , $telegram );
             }
         }
+        if (isset($_POST['addchild'])) {
+            $child =intval( $_POST['child']);
+            $id_order = htmlentities($_POST['id_order']) ? htmlentities($_POST['id_order']) : null;
+            if ($child!=null&&   $id_order!=null){
+                $user_child = User::searchUser($child);
+                $order=Order::getOrder($order_id);
+                if($user_child!=false){
+                    foreach($items as $item) {
+                    $product = Product::getProductDataForSendOrder($item['product_id']);
+                    if ($product['manager_letter'] != null) {
+                        $manager_letter = unserialize(base64_decode($product['manager_letter']));
+                        if (isset($manager_letter['email_manager']) && !empty($manager_letter['email_manager'])) {
+                            $subj_manager = isset($manager_letter['subj_manager']) ? $manager_letter['subj_manager'] : null;
+                            $letter_manager = isset($manager_letter['letter_manager']) ? $manager_letter['letter_manager'] : null;
+                            $send_custom = Email::sendCustomLetterForManager($manager_letter['email_manager'],
+                                $subj_manager, $letter_manager, $order
+                            );
+                        }
+                    }
+                    if ($product['del_group_id']) {
+                        User::deleteUserGroupsFromList($user_child['user_id'], $product['del_group_id']);
+                    }
+                    // Добавление групп для пользователя при рассрчоке и БЕЗ
+                    if ($product['group_id'] != 0 && ($order['installment_map_id'] == 0 || $product['installment_addgroups'] == 0)) {
+                        $add_groups = explode(",", $product['group_id']);
+                        foreach ($add_groups as $group) {
+                            User::WriteUserGroup($user_child['user_id'], $group);
+                        }
+                    }
+
+                    $training = System::CheckExtensension('training', 1);
+                    if ($training) {
+                        $user_groups = $user_child['user_id'] ? User::getGroupByUser($user_child['user_id']) : false;
+                        $user_planes = $user_child['user_id'] ? Member::getPlanesByUser($user_child['user_id'], 1) : false;
+                        if ($user_groups || $user_planes) {
+                            $filter = [
+                                'user_groups' => $user_groups,
+                                'user_planes' => $user_planes
+                            ];
+                            $training_list = $user_child['user_id'] ? Training::getTrainingList(null, null, $filter, null) : null;
+                            if ($training_list) {
+                                foreach($training_list as $training) {
+                                    if ($training['curators_auto_assign']==1) {
+                                        Order::AssignUserToCurator($user_child['user_id'], $training);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    $subscription_id=null;
+                    $membership = System::CheckExtensension('membership', 1);
+                    if ($membership && $user_child && !empty($product['subscription_id']) && ($order['installment_map_id'] == 0 )) {
+                        Member::renderMember($product['subscription_id'], $user_child['user_id'], 1, $subscription_id, $order['subs_id']);
+                    }
+
+                }
+                if ($user_child) {
+                    if (isset($_SESSION['confirm_phone'])) {
+                        User::confirmPhone($user_child['user_id'], $phone);
+                    }
+
+                    $_SESSION['reg_status'] = 1;
+
+                    if ($custom_fields) {
+                        $custom_fields_data = isset($_POST['custom_fields']) ? $_POST['custom_fields'] : [];
+                        CustomFields::saveUserFields($user_child['user_id'], null, $custom_fields_data);
+                    }
+
+                    
+                }
+                ToChild::close($order_id,$child);
+                }
+                else{
+                    ErrorPage::returnError('Пользователя с таким email нет в системе');
+                }
+                
+
+            }
+        }
+
+
         if (isset($_POST['save_req'])) {
             if (isset($_POST['req'])) {
                 foreach($_POST['req'] as $key => $value) {
