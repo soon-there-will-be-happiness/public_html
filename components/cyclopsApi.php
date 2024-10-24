080f19939e1a97a2888befa82273a76f7eabd5d4
carpediem <karsakovkirilo@gmail.com>
2024-10-24 13:49:53 +0200

    edit metod and add transfer_money function

--- components/cyclopsApi.php
+++ components/cyclopsApi.php
@@ -8,7 +8,10 @@ class cyclopsApi {
 
     public function __construct() {
         // Load environment variables from .env or configuration
-        $this->apiUrl = 'https://pre.tochka.com/api/v1/cyclops/v2/jsonrpc';
+        $this->apiUrl = [
+            'jsonrpc' => 'https://pre.tochka.com/api/v1/cyclops/v2/jsonrpc',
+            'tender-helpers' => 'https://pre.tochka.com/api/v1/tender-helpers/'
+            ];
         $this->signSystem = $_ENV['SIGN_SYSTEM'];
         $this->signThumbprint = $_ENV['SIGN_THUMBPRINT'];
         Log::add(0,'Debug', ["system" => $_ENV['SIGN_SYSTEM'], "t" => $_ENV['SIGN_THUMBPRINT']],'cyclops.log');
@@ -23,7 +26,7 @@ class cyclopsApi {
         return self::$instance;
     }
 
-    public function makeRequest($method, $params) {
+    public function makeRequest($api, $method, $params) {
         $payload = json_encode([
             "jsonrpc" => "2.0",
             "method" => $method,
@@ -34,12 +37,12 @@ class cyclopsApi {
         $headers = [
             'Content-Type: application/json',
             'sign-system: ' . $this->signSystem,
-            'sign-data: ' . '12345',
+            'sign-data: ' . hash('sha256', $payload),
             'sign-thumbprint: ' . $this->signThumbprint,
         ];
 
         // Initialize cURL
-        $ch = curl_init($this->apiUrl);
+        $ch = curl_init($this->apiUrl[$api]);
 
         // Set options for cURL request
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -64,6 +67,19 @@ class cyclopsApi {
 
     public function echo($text) {
         $params = ["text" => $text];
-        return $this->makeRequest('echo', $params);
+        return $this->makeRequest('jsonrpc','echo', $params);
+    }
+
+    # BETA-function for simulation transact
+    public function transfer_money($amount,$purpose = "ТЕСТОВЫЙ СЛОЙ - Перевод денег, без НДС", $payer_account=40702810713500000456, $payer_bank_code=044525104) {
+        $params = [
+            "recipient_account" => $_ENV['NOMINAL_ACCOUNT'],
+            "recipient_bank_code" => $_ENV['BIC'],
+            "amount" => $amount,
+            "purpose" => $purpose,
+            "payer_account" => $payer_account,
+            "payer_bank_code" => $payer_bank_code,
+            ];
+        return $this->makeRequest('tender-helpers','transfer_money', $params);
     }
 }
