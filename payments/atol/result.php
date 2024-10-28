80ff1d10bd1dc528d28600c3c904497f92d53ab2
Daniil Ivashchev <ivasevdaniil@gmail.com>
2024-10-28 14:59:58 +0200

    fix: add log

--- payments/atol/result.php
+++ payments/atol/result.php
@@ -4,12 +4,13 @@
 
 // Чтение и декодирование данных callback
 $callback_data = json_decode(file_get_contents('php://input'), true);
-Log::add(2,'Curl error', ["error" => $callback_data],'return.log');
+Log::add(0,'Curl error', ["error" => $callback_data],'return.log');
+Log::add(5,'Curl callback_data', context: ["callback_data" => $callback_data],type: 'return.log');
 // Проверка необходимых параметров
 if (isset($callback_data['orderId']) && isset($callback_data['status'])) {
-    $order_id = $callback_data['orderId'];
+    $order_id = intval($callback_data['orderId']);
     $status = $callback_data['status'];
-    Log::add(3,'Curl error', ["error" => $status],'return.log');
+    Log::add(5,'Curl error', ["error" => $status],'return.log');
 
     // Получение данных заказа
     $order = Order::getOrderDataByID($order_id,100);
