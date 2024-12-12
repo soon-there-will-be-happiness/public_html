65d9e9408492578ac626474b2838efba39b1e2cc
Daniil Ivashchev <ivasevdaniil@gmail.com>
2024-12-12 18:56:19 +0200

    fix phone

--- models/autotoken.php
+++ models/autotoken.php
@@ -157,7 +157,7 @@ class AutoToken {
                         "type" => "another",
                     ],
                     "supplier_info" => [
-                        "phones" => [(string) $phone,],
+                        "phones" => [(string)$user['phone'],],
                         "name" => $data['rs']['off_name'],
                         "inn" => (string)$data['rs']['inn'],
                     ],
