f27c2213777c5e1aadedd7b8ecdaa3220ab5d136
Daniil Ivashchev <ivasevdaniil@gmail.com>
2024-12-17 23:52:46 +0200

    add code to indentificate pay

--- components/cyclops_cron.php
+++ components/cyclops_cron.php
@@ -69,3 +69,29 @@ if (isset($response['result']['payment']) && !empty($response['result']['payment
         }
     }
 }
+
+$matched_payments=Payments::getJoinedOrdersAndPayments();
+$payments_tochkas=Payments::getAllPaymentsTochka(status: 'unmatched');
+foreach ($payments_tochkas as $payments_tochka) {
+    $i=0;
+    $amount=floatval($payments_tochka["amount"]);
+    foreach($matched_payments as $matched_payment){
+        if( $amount<=0){
+            break;
+        }else{
+            $amount-=floatval($matched_payment["amount"]);
+        }
+    }
+    if($i>0){
+        Payments::updatePaymentStatus($payments_tochka['id'],'matched');
+        for($j=0;$j<$i;$j++){
+            Payments::updatePaymentId($matched_payments[$j]['id'],$payments_tochka['id']);
+        }
+    }
+    else{
+        Log::add(1, 'unmatched pay', [
+            'payments_tochka' => $payments_tochka['id'],
+            ], 'cyclops_match');
+    }
+}
+
