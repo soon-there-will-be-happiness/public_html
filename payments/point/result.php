161f6259089640b902508c7dd06b9fe3f05e3022
Daniil Ivashshev <dapashlivy9966@gmail.com>
2025-03-12 11:06:48 +0200

    update result to check pay

--- payments/point/result.php
+++ payments/point/result.php
@@ -4,11 +4,10 @@ $params = unserialize(base64_decode($payment['params']));
 $order_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if($order_id !=0){
 
-   $order = Order::getOrder($order_id);
+$order = Order::getOrder($order_id);
 $point=PointDB::findRecordByOrderId($order_id );
 $setting = System::getSetting(true);
-if($point['status']!=true)
-{
+
     $api_url = $params['url'];
     $token = $params['token'];
 
@@ -39,11 +38,9 @@ if($point['status']!=true)
         PointDB::updateStatusToTrue($order_id);
         AutoToken::sendCheck( $order);
     }
-}
+
 System::redirectUrl($setting['script_url'] . '/payments/point/success?id='.$order_id); 
-}
-else{
-    System::redirectUrl($setting['script_url'] . '/payments/point/success?id='.$order_id); 
-}
+
+
 
 ?>
