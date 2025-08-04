34c853338806c061b58261c5b05a2d2affc88e62
Daniil Ivashshev <dapashlivy9966@gmail.com>
2025-08-04 22:55:40 +0300

    Add CSS styles for popup and button components
    - Introduced styles for a buy button with specific dimensions and colors.
    - Added styles for a popup modal including visibility control, layout, and content styling.
    - Implemented responsive design adjustments for various screen sizes.
    - Included styles for form elements, input fields, and promotional blocks.
    - Enhanced text styling for titles and subtitles within the popup.
    - Added hidden class for managing element visibility.

--- config/routes.php
+++ config/routes.php
@@ -66,9 +66,8 @@ return [
     'st/kemstat/page' => 'site/kemstat',
 
     'st/free/page' => 'site/free',
-    'kemstat7_11' => 'site/project',
-    'kemstat12_17' => 'site/project',
-
+    'st/kemstat7_11/page' => 'site/project',
+    'st/kemstat12_17/page' => 'site/project',
     'st/ambassador/page' => 'site/ambassador',
 
     'oferta?id=([0-9]+)' => 'site/oferta/$1',
